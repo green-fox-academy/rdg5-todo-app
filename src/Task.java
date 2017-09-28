aa217d5194bc0e2427dfc4c21f7f8377b7025a0d
Sándor Vass <vasssandor@hotmail.com>
2017-09-28 11:54:10 +0200

    init Task class

---
+++ src/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+
+  private String task;
+  private boolean completed;
+
+  public Task(String taskToDo) {
+    this.task = taskToDo;
+  }
+
+  public void complete() {
+    this.completed = true;
+  }
+
+  @Override
+  public String valami() {
+    return (completed ? "[x] " : "[ ] ") + task;
+  }
+}
--- src/todoApp.java
+++ src/todoApp.java
@@ -1,29 +1,56 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class todoApp {
 
   String task;
 
 
   public static void main(String[] args) {
-    printUsage();
+//    printUsage();
+//    listReader();
+//    taskAdder("jóska");
 //    addTask()
 
-  }
+  //}
 
-
-
-//  public void addTask(String task) {
-//    this.task = task;
+//  public static void taskAdder(String task) {
+//    List<String> tasks = new ArrayList();
+//    tasks.add(task);
+//    try {
+//      Path filePath = Paths.get("tasks.txt");
+//      Files.write(filePath, tasks);
+//    } catch (Exception e) {
+//      System.out.println("Uh-oh, could not write the file!");
+//    }
 //  }
-
-  public static void printUsage() {
-    System.out.println("Command Line Todo application\n"
-        + "=============================\n"
-        + "\n"
-        + "Command line arguments:\n"
-        + " -l   Lists all the tasks\n"
-        + " -a   Adds a new task\n"
-        + " -r   Removes an task\n"
-        + " -c   Completes an task");
-  }
-}
-
+//
+//  public static void listReader() {
+//    try {
+//      Path filePath = Paths.get("tasks.txt");
+//      List<String> lines = Files.readAllLines(filePath);
+//      System.out.println(lines.get(0));
+//    } catch (Exception e) {
+//      System.out.println("No todos for today! :)");
+//    }
+//  }
+//
+////  public void addTask(String task) {
+////    this.task = task;
+////  }
+//
+//  public static void printUsage() {
+//    System.out.println("Command Line Todo application\n"
+//        + "=============================\n"
+//        + "\n"
+//        + "Command line arguments:\n"
+//        + " -l   Lists all the tasks\n"
+//        + " -a   Adds a new task\n"
+//        + " -r   Removes an task\n"
+//        + " -c   Completes an task");
+//  }
+//}
+//
