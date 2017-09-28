78db6b255deb5232b1caa1d29bc3f53e7e06be14
Sándor Vass <vasssandor@hotmail.com>
2017-09-28 11:05:41 +0200

    init textfile for the tasks

--- src/todoApp.java
+++ src/todoApp.java
@@ -1,13 +1,29 @@
 public class todoApp {
 
+  String task;
+
+
   public static void main(String[] args) {
+    printUsage();
+//    addTask()
+
+  }
+
+
+
+//  public void addTask(String task) {
+//    this.task = task;
+//  }
+
+  public static void printUsage() {
     System.out.println("Command Line Todo application\n"
         + "=============================\n"
         + "\n"
         + "Command line arguments:\n"
         + " -l   Lists all the tasks\n"
         + " -a   Adds a new task\n"
-        + " -r   Removes a task\n"
-        + " -c   Completes a task");
+        + " -r   Removes an task\n"
+        + " -c   Completes an task");
   }
 }
+
