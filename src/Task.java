70a6e972fefa72c749256c83e8d6058948a68e65
Sándor Vass <vasssandor@hotmail.com>
2017-09-28 13:45:35 +0200

    implemented list reading, moving to task writing

--- src/Task.java
+++ src/Task.java
@@ -1,18 +1,19 @@
-public class Task {
-
-  private String task;
-  private boolean completed;
-
-  public Task(String taskToDo) {
-    this.task = taskToDo;
-  }
-
-  public void complete() {
-    this.completed = true;
-  }
-
-  @Override
-  public String valami() {
-    return (completed ? "[x] " : "[ ] ") + task;
-  }
-}
+//public class Task {
+//
+//  private String task;
+//  private boolean completed;
+//
+//  public Task(String taskToDo) {
+//
+//    this.task = taskToDo;
+//  }
+//
+//  public void complete() {
+//    this.completed = true;
+//  }
+//
+//  @Override
+//  public String valami() {
+//    return (completed ? "[x] " : "[ ] ") + task;
+//  }
+//}
--- src/todoApp.java
+++ src/todoApp.java
@@ -1,56 +1,63 @@
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class todoApp {
 
   String task;
+  boolean completed;
 
 
   public static void main(String[] args) {
-//    printUsage();
-//    listReader();
-//    taskAdder("jóska");
-//    addTask()
+    printUsage();
+    taskReader();
+//    taskAdder(new String[]{"jóskafiú"});
 
-  //}
 
-//  public static void taskAdder(String task) {
-//    List<String> tasks = new ArrayList();
-//    tasks.add(task);
-//    try {
-//      Path filePath = Paths.get("tasks.txt");
-//      Files.write(filePath, tasks);
-//    } catch (Exception e) {
-//      System.out.println("Uh-oh, could not write the file!");
-//    }
-//  }
-//
-//  public static void listReader() {
-//    try {
-//      Path filePath = Paths.get("tasks.txt");
-//      List<String> lines = Files.readAllLines(filePath);
-//      System.out.println(lines.get(0));
-//    } catch (Exception e) {
-//      System.out.println("No todos for today! :)");
-//    }
-//  }
-//
-////  public void addTask(String task) {
-////    this.task = task;
-////  }
-//
-//  public static void printUsage() {
-//    System.out.println("Command Line Todo application\n"
-//        + "=============================\n"
-//        + "\n"
-//        + "Command line arguments:\n"
-//        + " -l   Lists all the tasks\n"
-//        + " -a   Adds a new task\n"
-//        + " -r   Removes an task\n"
-//        + " -c   Completes an task");
-//  }
-//}
-//
+  }
+
+
+  public static void taskAdder(String[] args) {
+    try {
+      Path listPath = Paths.get("tasks.txt");
+      List<String> lines = Files.readAllLines(listPath);
+      lines.add(args[1]);
+      Files.write(listPath, lines, Charset.defaultCharset());
+    } catch (Exception e) {
+      System.out.println("Uh-oh, could not write the file!");
+    }
+  }
+
+
+  public static void taskReader() {
+    try {
+      Path listPath = Paths.get("tasks.txt");
+      List<String> lines = Files.readAllLines(listPath);
+      int taskNumber = 1;
+      if (lines.get(0) != null) {
+        for (int i = 0; i < lines.size(); i++) {
+          System.out.println(taskNumber + " - " + lines.get(i));
+          taskNumber++;
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("No todos for today! :)");
+    }
+  }
+
+
+  public static void printUsage() {
+    System.out.println("Command Line Todo application\n"
+        + "=============================\n"
+        + "\n"
+        + "Command line arguments:\n"
+        + " -l   Lists all the tasks\n"
+        + " -a   Adds a new task\n"
+        + " -r   Removes an task\n"
+        + " -c   Completes an task");
+  }
+}
