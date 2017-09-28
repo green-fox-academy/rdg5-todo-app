bc8ea018f9f5f230dbec4eafbc4a608c22a22262
Sándor Vass <vasssandor@hotmail.com>
2017-09-28 10:49:51 +0200

    init print usage, also works in bash

--- src/todoApp.java
+++ src/todoApp.java
@@ -1,3 +1,13 @@
 public class todoApp {
 
+  public static void main(String[] args) {
+    System.out.println("Command Line Todo application\n"
+        + "=============================\n"
+        + "\n"
+        + "Command line arguments:\n"
+        + " -l   Lists all the tasks\n"
+        + " -a   Adds a new task\n"
+        + " -r   Removes a task\n"
+        + " -c   Completes a task");
+  }
 }
